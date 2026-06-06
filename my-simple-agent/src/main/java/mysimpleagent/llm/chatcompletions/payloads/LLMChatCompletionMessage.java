af91aa270567999da3cd956945a4d2f8cf74275a
hbobenicio <hbobenicio@gmail.com>
2026-06-06 09:55:54 -0300

    deleting my-simple-agent (moved to its own repo)

--- my-simple-agent/src/main/java/mysimpleagent/llm/chatcompletions/payloads/LLMChatCompletionMessage.java
+++
@@ -1,5 +0,0 @@
-package mysimpleagent.llm.chatcompletions.payloads;
-
-//TODO should I add tool calls here?
-public record LLMChatCompletionMessage(String role, String content) {
-}
--- my-simple-agent/src/main/java/mysimpleagent/llm/chatcompletions/payloads/LLMPromptTokensDetails.java
+++
@@ -1,8 +0,0 @@
-package mysimpleagent.llm.chatcompletions.payloads;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record LLMPromptTokensDetails(
-        @JsonProperty("cached_tokens") int cachedTokens
-) {
-}
